967d04785c8a963f033a52bb0f2fbe55869c8cf0
Adam Creusevault <adam.creusevault@etu.univ-nantes.fr>
2023-12-05 10:00:09 +0100

    Created some getters

--- main.py
+++ main.py
@@ -8,10 +8,8 @@ class Block:
         self._date = datetime.datetime.now()
         self._hash_value = hashlib.new('sha256') # sha256 of prev_hash + transaction + date
      
-        if(type(self._prev_hash) == str):
-            value_to_hash: str = self._prev_hash + self._transaction + self._date.strftime('%Y-%m-%d %H:%M:%S')
-        else:
-            value_to_hash: str = self._prev_hash.hexdigest() + self._transaction + self._date.strftime('%Y-%m-%d %H:%M:%S')
+        value_to_hash: str = self._prev_hash + self._transaction + self._date.strftime('%Y-%m-%d %H:%M:%S')
+        
             
         self._hash_value.update( value_to_hash.encode('utf-8')) # string.encode('utf-8')
 
@@ -19,15 +17,18 @@ class Block:
         self._hash_value.hexdigest()
 
 
+    def get_hash(self) -> str:
+        return self._hash_value.hexdigest()
 
-
+    def getTransaction(self) -> str:
+        return self._transaction
 
 
 if __name__ == "__main__":
     b1 = Block("", "Alice gave 5€ to Bob")
     
-    b2 = Block(b1._hash_value, "Bob paid a pizza 18 € to Michel")
+    b2 = Block(b1.get_hash(), "Bob paid a pizza 18 € to Michel")
 
-    print(b1._hash_value.hexdigest())
-    print(b2._hash_value.hexdigest())
+    print(b1.get_hash())
+    print(b2.get_hash())
     pass
