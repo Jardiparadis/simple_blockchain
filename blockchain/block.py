f28ae8672f00a4a615f0c10237a85955eba56f18
Adam Creusevault <adam.creusevault@etu.univ-nantes.fr>
2023-12-05 10:03:18 +0100

    We created the blockchain package

---
+++ blockchain/block.py
@@ -0,0 +1,25 @@
+import datetime
+import hashlib
+
+class Block:
+    def __init__(self, prev_hash, transaction) -> None:
+        self._prev_hash = prev_hash
+        self._transaction = transaction
+        self._date = datetime.datetime.now()
+        self._hash_value = hashlib.new('sha256') # sha256 of prev_hash + transaction + date
+     
+        value_to_hash: str = self._prev_hash + self._transaction + self._date.strftime('%Y-%m-%d %H:%M:%S')
+        
+            
+        self._hash_value.update( value_to_hash.encode('utf-8')) # string.encode('utf-8')
+
+
+        self._hash_value.hexdigest()
+
+
+    def get_hash(self) -> str:
+        return self._hash_value.hexdigest()
+
+    def get_transaction(self) -> str:
+        return self._transaction
+
--- main.py
+++ main.py
@@ -1,28 +1,4 @@
-import datetime
-import hashlib
-
-class Block:
-    def __init__(self, prev_hash, transaction) -> None:
-        self._prev_hash = prev_hash
-        self._transaction = transaction
-        self._date = datetime.datetime.now()
-        self._hash_value = hashlib.new('sha256') # sha256 of prev_hash + transaction + date
-     
-        value_to_hash: str = self._prev_hash + self._transaction + self._date.strftime('%Y-%m-%d %H:%M:%S')
-        
-            
-        self._hash_value.update( value_to_hash.encode('utf-8')) # string.encode('utf-8')
-
-
-        self._hash_value.hexdigest()
-
-
-    def get_hash(self) -> str:
-        return self._hash_value.hexdigest()
-
-    def getTransaction(self) -> str:
-        return self._transaction
-
+from blockchain.block import Block
 
 if __name__ == "__main__":
     b1 = Block("", "Alice gave 5€ to Bob")
