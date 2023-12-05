ba94b6b57232c0dcc2e0dc7e6167688be6aa068e
Adam Creusevault <adam.creusevault@etu.univ-nantes.fr>
2023-12-05 09:55:29 +0100

    The blocks are linked with hash value

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 import datetime
+import hashlib
 
 class Block:
     def __init__(self, prev_hash, transaction) -> None:
         self._prev_hash = prev_hash
         self._transaction = transaction
         self._date = datetime.datetime.now()
-        self._hash_value = 0 # sha256 of prev_hash + transaction + date 
+        self._hash_value = hashlib.new('sha256') # sha256 of prev_hash + transaction + date
+     
+        if(type(self._prev_hash) == str):
+            value_to_hash: str = self._prev_hash + self._transaction + self._date.strftime('%Y-%m-%d %H:%M:%S')
+        else:
+            value_to_hash: str = self._prev_hash.hexdigest() + self._transaction + self._date.strftime('%Y-%m-%d %H:%M:%S')
+            
+        self._hash_value.update( value_to_hash.encode('utf-8')) # string.encode('utf-8')
+
+
+        self._hash_value.hexdigest()
+
+
+
+
 
 
 if __name__ == "__main__":
-    b1 = Block(None, "Alice gave 5€ to Bob")
-    b2 = Block(b1, "Bob paid a pizza 18 € to Michel")
+    b1 = Block("", "Alice gave 5€ to Bob")
+    
+    b2 = Block(b1._hash_value, "Bob paid a pizza 18 € to Michel")
 
-
+    print(b1._hash_value.hexdigest())
+    print(b2._hash_value.hexdigest())
     pass
