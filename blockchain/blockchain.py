8fe0ee0a3fd57dd54ccc78bde7d8cb1410d69343
a@a.a <a@a.a>
2023-12-05 16:22:24 +0100

    type functions prototype

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -6,7 +6,7 @@ class Blockchain:
     def __init__(self):
         self.chain = []
 
-    def add_transaction(self, transaction) -> str:
+    def add_transaction(self, transaction: list[str]) -> str:
         previous_hash = ""
         if len(self.chain) != 0:
             previous_hash = self.chain[-1].get_hash()
@@ -14,12 +14,12 @@ class Blockchain:
         self.chain.append(new_block)
         return new_block.get_hash()
 
-    def get_transaction(self, hash_value):
+    def get_transaction(self, hash_value: str) -> str:
         for block in self.chain:
             if block.get_hash() == hash_value:
                 return block.get_transaction()
 
-    def get_token_for_user(self, public_key):
+    def get_token_for_user(self, public_key: str) -> int:
         user_tokens = 0
 
         for block in self.chain:
@@ -31,11 +31,15 @@ class Blockchain:
                     user_tokens -= int(nb_tokens)
         return user_tokens
 
-    def create_token(self, public_key, nb_tokens):
+    def create_token(self, public_key: bytes, nb_tokens: int):
         # sender "#" does not exist, these tokens appeared magically
         self.add_transaction([f"#:{public_key.decode()}:{nb_tokens}"])
 
-    def transfer_token(self, sender_key, receiver_key, nb_tokens, message, signature):
+    """
+    Transfer {nb_tokens} tokens from the user with the key {sender_key} to the user with the key {receiver_key}
+    Message and signature are sent to authenticate the sender 
+    """
+    def transfer_token(self, sender_key: bytes, receiver_key: bytes, nb_tokens: int, message: bytes, signature: bytes):
         # Check if signature is verified
         try:
             sender_public_key = rsa.PublicKey.load_pkcs1(sender_key)
@@ -46,10 +50,11 @@ class Blockchain:
         sender_tokens = self.get_token_for_user(sender_key.decode())
         if sender_tokens < nb_tokens:
             return print('Sender has not enough token, abort token transfer.')
-        # Transfer tokens
+        # Transfer tokens: the transfer data is stores as a string with the following format:
+        # "sender_key:receiver_key:nb_tokens"
         self.add_transaction([f"{sender_key.decode()}:{receiver_key.decode()}:{nb_tokens}"])
 
-    def get_tokens_per_user(self):
+    def get_tokens_per_user(self) -> dict:
         nb_tokens_per_user = {}
 
         for block in self.chain:
@@ -62,4 +67,7 @@ class Blockchain:
                 nb_tokens_per_user[receiver] += int(nb_tokens)
                 nb_tokens_per_user[sender] -= int(nb_tokens)
 
+        # Ignore the user "#", it is used to insert new tokens in the blockchain from nothing
+        if "#" in nb_tokens_per_user:
+            del nb_tokens_per_user["#"]
         return nb_tokens_per_user
