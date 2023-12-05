39078ee563704389879325aa0ba7fd4df8e8c2ae
a@a.a <a@a.a>
2023-12-05 15:29:05 +0100

    token transfer with key verification and wallet check

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -1,5 +1,5 @@
 from blockchain.block import Block
-
+import rsa
 
 class Blockchain:
     def __init__(self):
@@ -18,10 +18,37 @@ class Blockchain:
             if block.get_hash() == hash_value:
                 return block.get_transaction()
 
-    def create_fake_initial_block(self, sender, receiver, nb_tokens):
-        self.add_transaction([f"{sender}:{receiver}:{nb_tokens}"])
+    def get_token_for_user(self, public_key):
+        user_tokens = 0
 
-    def get_statistics(self):
+        for block in self.chain:
+            for transaction in block.get_transaction():
+                sender, receiver, nb_tokens = transaction.split(':')
+                if receiver == public_key:
+                    user_tokens += int(nb_tokens)
+                if sender == public_key:
+                    user_tokens -= int(nb_tokens)
+        return user_tokens
+
+    def create_token(self, public_key, nb_tokens):
+        # send # does not exist, these token appeared magically
+        self.add_transaction([f"#:{public_key.decode()}:{nb_tokens}"])
+
+    def transfer_token(self, sender_key, receiver_key, nb_tokens, message, signature):
+        # Check if signature is verified
+        try:
+            sender_public_key = rsa.PublicKey.load_pkcs1(sender_key)
+            rsa.verify(message, signature, sender_public_key)
+        except rsa.pkcs1.VerificationError:
+            return print('Verification failed, abort token transfer.')
+        # Check is sender has enough tokens
+        sender_tokens = self.get_token_for_user(sender_key.decode())
+        if sender_tokens < nb_tokens:
+            return print('Sender has not enough token, abort token transfer.')
+        # Transfer tokens
+        self.add_transaction([f"{sender_key.decode()}:{receiver_key.decode()}:{nb_tokens}"])
+
+    def get_tokens_per_user(self):
         nb_tokens_per_user = {}
 
         for block in self.chain:
--- main.py
+++ main.py
@@ -1,9 +1,22 @@
+import hashlib
 from blockchain.blockchain import Blockchain
+import rsa
+
 
 if __name__ == "__main__":
     blockchain = Blockchain()
 
-    blockchain.create_fake_initial_block("A", "B", 12)
-    blockchain.create_fake_initial_block("C", "B", 12)
+    public_key_a, private_key_a = rsa.newkeys(512)
+    pksc_a = public_key_a.save_pkcs1()
 
-    print(blockchain.get_statistics())
+    public_key_b, private_key_b = rsa.newkeys(512)
+    pksc_b = public_key_b.save_pkcs1()
+
+    signature_a = rsa.sign(b"user1", private_key_a, "SHA-1")
+    signature_b = rsa.sign(b"user2", private_key_b, "SHA-1")
+
+    blockchain.create_token(pksc_a, 20)
+    blockchain.create_token(pksc_b, 20)
+
+    blockchain.transfer_token(pksc_a, pksc_b, 12, b"user1", signature_a)
+    blockchain.transfer_token(pksc_b, pksc_a, 4, b"user2", signature_b)
