1e52bf021834da21ad0e582cdc9cb6f91c10769b
a@a.a <a@a.a>
2023-12-05 16:56:46 +0100

    create Network and Node class

--- main.py
+++ main.py
@@ -1,26 +1,36 @@
 import hashlib
 from blockchain.blockchain import Blockchain
 import rsa
+import copy
+
+
+class Node:
+    def __init__(self, blockchain, public_key, private_key):
+        self.public_key = public_key
+        self.private_key = private_key
+        # Each node has its own blockchain copy
+        self.blockchain = copy.deepcopy(blockchain)
+
+
+class Network:
+    def __init__(self):
+        self.nodes = []
+
+    def register_nodes(self, node):
+        self.nodes.append(node)
+
+    def transfer_token(self):
+        pass
 
 
 if __name__ == "__main__":
-    blockchain = Blockchain()
+    network = Network()
 
     public_key_a, private_key_a = rsa.newkeys(512)
     # PKSC = Public Key Cryptographic Standards https://fr.wikipedia.org/wiki/Public_Key_Cryptographic_Standards
     pksc_a = public_key_a.save_pkcs1()
+    network.register_nodes(Node(Blockchain(), public_key_a, private_key_a))
 
     public_key_b, private_key_b = rsa.newkeys(512)
     pksc_b = public_key_b.save_pkcs1()
-
-    signature_a = rsa.sign(b"user1", private_key_a, "SHA-1")
-    signature_b = rsa.sign(b"user2", private_key_b, "SHA-1")
-
-    # Create magic tokens to test transfer
-    blockchain.create_token(pksc_a, 20)
-    blockchain.create_token(pksc_b, 20)
-
-    blockchain.transfer_token(pksc_a, pksc_b, 12, b"user1", signature_a)
-    blockchain.transfer_token(pksc_b, pksc_a, 4, b"user2", signature_b)
-
-    print(blockchain.get_tokens_per_user())
+    network.register_nodes(Node(Blockchain(), public_key_b, private_key_b))
